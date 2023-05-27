b223b3b80a5acd5e214a7e05ded65f55a2757d77
visprogithub <60151642+visprogithub@users.noreply.github.com>
2023-05-27 01:53:15 -0400

    Create home.ts

---
+++ home.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.html',
+  styleUrls: ['home.css'],
+})
+export class Home {
+  rawaf5w: string = ' '
+  rawywfy: string = ' '
+  rawvq44: string = ' '
+  raw4pyv: string = ' '
+  raw041g: string = ' '
+  rawdaqo: string = ' '
+  raw2fx7: string = ' '
+  rawk25s: string = ' '
+  raw51bi: string = ' '
+  constructor() {}
+}
